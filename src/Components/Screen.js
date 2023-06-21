582d682d73a2c39ce6200f9904f0ab1f6ebe4c05
Nika <nikaonime@gmail.com>
2023-06-21 11:28:47 +0300

    react-textfit

--- src/Components/Screen.js
+++ src/Components/Screen.js
@@ -1,12 +1,13 @@
 import React, { useContext } from 'react'
 import { CalcContext } from '../Context/CalcContext';
+import { Textfit } from 'react-textfit';
 
 const Screen = () => {
 
     const { calc } = useContext(CalcContext)
 
     return (
-        <div className='screen'>Screen</div>
+        <Textfit className='screen' max={70} mode={'single'}>{calc.num ? calc.num : calc.res}</Textfit>
     )
 }
 
