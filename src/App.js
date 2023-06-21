f6f0d558d2973156a71cd1254092de6f8ddbf053
Nika <nikaonime@gmail.com>
2023-06-21 09:48:44 +0300

    Screen component(+css)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './index.css';
 import Wrapper from './Components/Wrapper';
+import Screen from './Components/Screen';
 
 function App() {
   return (
     <div>
       <Wrapper>
         <h1>app</h1>
+        <Screen />   {/* тут будут цыферки писаться */}
       </Wrapper>
     </div>
   );
---
+++ src/Components/Screen.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Screen = () => {
+    return (
+        <div className='screen'>Screen</div>
+    )
+}
+
+export default Screen;
